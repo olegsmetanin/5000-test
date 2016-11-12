5ea8f8ab6f50f61eec813a3c95e2266afbfb0a1c
Iogansen Denis <pofigizm@gmail.com>
2016-11-12 12:26:49 +0300

    add perf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Perf from 'react-addons-perf'
 
 const arr = size => new Array(size).fill(0)
 
-const Inp = ({ value, ix, dispatch }) => (
-  <input
-    value={ value }
-    onChange={ ev => dispatch({ type: 'CHANGE', ix, value: ev.target.value }) }
-    style={{ display: 'block' }}
-  />
-)
-const wrap = ix => connect( st => ({ ...st.data[ix], ix }) )(Inp)
+class Inp extends React.Component {
+  change(args) {
+    Perf.start()
+    setTimeout(() => {
+      Perf.stop()
+      Perf.printInclusive()
+      Perf.printExclusive()
+      Perf.printWasted()
+      Perf.printOperations()
+    }, 1000)
+    this.props.dispatch({ type: 'CHANGE', ...args })
+  }
+  render () {
+    const { ix, data } = this.props
+    return (
+      <input
+        value={ data.value }
+        onChange={ ev => this.change({ ix, value: ev.target.value }) }
+        style={{ display: 'block' }}
+      />
+    )
+  }
+}
+const wrap = ix => connect( st => ({ data: st.data[ix], ix }) )(Inp)
 
 const App = ({ size }) => (
   <div>
