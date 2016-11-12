8d98f8a16a3fca33c9a589e43b142df31d5085a6
Iogansen Denis <pofigizm@gmail.com>
2016-11-12 09:42:12 +0300

    simple wrap pure

---
+++ README.md
@@ -0,0 +1,3 @@
+[Превью](
+https://pofigizm.github.io/5000-test/) для прод сборки [простого решения](https://github.com/Pofigizm/5000-test/tree/ec83ea812c8ed2e960dc813d1e646d9641444366)
+
--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import shallowEqual from 'fbjs/lib/shallowEqual'
+
+class Inp extends React.Component {
+  shouldComponentUpdate (props, state) {
+    return !shallowEqual(this.props.data, props.data)
+  }
+  render () {
+    const { ix, data, change } = this.props
+    return (
+      <input
+        value={ data.value }
+        onChange={ ev => change(ix, ev.target.value) }
+        style={{ display: 'block' }}
+      />
+    )
+  }
+}
 
 const App = ({
   store,
   change,
 }) => (
   <div>
-    { store.map(({ value }, ix) => (
-      <input
-        key={ ix }
-        value={ value }
-        onChange={ ev => change(ix, ev.target.value) }
-        style={{ display: 'block' }}
-      />
+    { store.map((data, ix) => (
+      <Inp {...{ key: ix, ix, data, change }} />
     )) }
   </div>
 )
